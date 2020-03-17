7a889f588871cbcf40ce501f040fe05e53acf9cf
huntedhappy <huntedhappy@gmail.com>
2020-03-17 13:32:27 +0900

    replace_patch_file

--- module_utils/sync_vars_to_tn.py
+++ module_utils/sync_vars_to_tn.py
@@ -8,8 +8,11 @@ with open('vars_files/deploy_nsx_tn_vars1.yml') as f:
     yaml = ruamel.yaml.YAML()  # defaults to round-trip if no parameters given
     code = yaml.load(f)
 
-code['transport_nodes'][0]['node_deployment_info']['deployment_config'] = vcenter_info['edge1_deployment_config']
-code['transport_nodes'][1]['node_deployment_info']['deployment_config'] = vcenter_info['edge2_deployment_config']
+code['transport_nodes'][0]['display_name'] = vcenter_info['edge1_display_name']
+code['transport_nodes'][1]['display_name'] = vcenter_info['edge2_display_name']
+code['transport_nodes'][0]['node_deployment_info'] = vcenter_info['node1_node_deployment_info']
+code['transport_nodes'][1]['node_deployment_info'] = vcenter_info['node2_node_deployment_info']
+code['edge_clusters'] = vcenter_info['edge_clusters']
 
 with open('vars_files/deploy_nsx_tn_vars1.yml', 'w') as f:
     yaml.default_flow_style=False
@@ -19,8 +22,12 @@ with open('vars_files/deploy_nsx_tn_vars2.yml') as f:
     yaml = ruamel.yaml.YAML()  # defaults to round-trip if no parameters given
     code = yaml.load(f)
 
-code['transport_nodes'][0]['node_deployment_info']['deployment_config'] = vcenter_info['edge1_deployment_config']
-code['transport_nodes'][1]['node_deployment_info']['deployment_config'] = vcenter_info['edge2_deployment_config']
+code['transport_nodes'][0]['display_name'] = vcenter_info['edge1_display_name']
+code['transport_nodes'][1]['display_name'] = vcenter_info['edge2_display_name']
+code['transport_nodes'][0]['node_deployment_info'] = vcenter_info['node1_node_deployment_info']
+code['transport_nodes'][1]['node_deployment_info'] = vcenter_info['node2_node_deployment_info']
+code['edge_clusters'] = vcenter_info['edge_clusters']
+
 
 with open('vars_files/deploy_nsx_tn_vars2.yml', 'w') as f:
     yaml.default_flow_style=False
@@ -30,8 +37,12 @@ with open('vars_files/deploy_nsx_tn_vars3.yml') as f:
     yaml = ruamel.yaml.YAML()  # defaults to round-trip if no parameters given
     code = yaml.load(f)
 
-code['transport_nodes'][0]['node_deployment_info']['deployment_config'] = vcenter_info['edge1_deployment_config']
-code['transport_nodes'][1]['node_deployment_info']['deployment_config'] = vcenter_info['edge2_deployment_config']
+code['transport_nodes'][0]['display_name'] = vcenter_info['edge1_display_name']
+code['transport_nodes'][1]['display_name'] = vcenter_info['edge2_display_name']
+code['transport_nodes'][0]['node_deployment_info'] = vcenter_info['node1_node_deployment_info']
+code['transport_nodes'][1]['node_deployment_info'] = vcenter_info['node2_node_deployment_info']
+code['edge_clusters'] = vcenter_info['edge_clusters']
+
 
 with open('vars_files/deploy_nsx_tn_vars3.yml', 'w') as f:
     yaml.default_flow_style=False
